fdb93adc6b603fd3e25d69f6ea1bccb206c208d2
Adil EL-HARRAS <adil.elharras@viseo.com>
2015-01-15 16:28:59 +0100

    fix register user

--- src/com/backlog/controller/UserBean.java
+++ src/com/backlog/controller/UserBean.java
@@ -2,19 +2,49 @@ package com.backlog.controller;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import com.backlog.dao.agency.UserDaoLocal;
+import com.backlog.model.User;
 
 @SuppressWarnings("serial")
 @ManagedBean(name = "userBean", eager = true)
 @SessionScoped
 public class UserBean implements Serializable {
-	private String name;
+	
+	private User user;
+	
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Inject
+	UserDaoLocal userDao;
+	
+	 public void AddUser() throws Exception {
+	        try {
+	        	System.out.println("Register......");
+	        	userDao.addUser(user);
+////	            facesContext.addMessage(null,
+////	                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
+//	            initMember();
+	        } catch (Exception e) {
+//	            String errorMessage = getRootErrorMessage(e);
+//	            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration Unsuccessful");
+//	            facesContext.addMessage(null, m);
+	        }
+	    }
 	 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	 @PostConstruct
+	    public void initMember() {
+	       user = new User();
+	    }
 }
